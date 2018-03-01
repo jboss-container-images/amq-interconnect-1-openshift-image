805addd6b372661a0b5fd4af510354e9d024e243
Kyle Liberti <kliberti@redhat.com>
2018-03-01 16:46:47 -0500

    WIP: Baseline Prototype

--- modules/interconnect/configure.sh
+++ modules/interconnect/configure.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-
 set -e
+
 SCRIPT_DIR=$(dirname $0)
+ADDED_DIR=${SCRIPT_DIR}/added
 
-#chown -R jboss:root ${JBOSS_HOME}
-#chmod -R g+rwX ${JBOSS_HOME}
+# Copy launch OpenShift scripts to container
+mkdir -p ${JBOSS_HOME}/bin/launch
+cp -r ${ADDED_DIR}/* ${JBOSS_HOME}/bin
 
-chown -R jboss:root ${SCRIPT_DIR}/configure.py
-chmod -R g+rwxX ${SCRIPT_DIR}/configure.py
+# Create location and set permissions for configuration files
+mkdir -p ${JBOSS_HOME}/conf/
+chown -R jboss:root ${JBOSS_HOME}/conf
+chmod -R g+rwx ${JBOSS_HOME}/conf
 
-cp ${SCRIPT_DIR}/configure.py ${JBOSS_HOME}/opt/
-python ${JBOSS_HOME}/opt/configure.py
-#OUTPUT="$(python ${SCRIPT_DIR}/con.py)"
-#echo "${OUTPUT}"
+chown -R jboss:root ${JBOSS_HOME}/bin/launch/configure.py
+chown -R jboss:root ${JBOSS_HOME}/bin/openshift-launch.sh
+
+chmod -R ug+x ${JBOSS_HOME}/bin/launch/configure.py
+chmod -R ug+x ${JBOSS_HOME}/bin/openshift-launch.sh
--- tests/features/interconnect.feature
+++ tests/features/interconnect.feature
@@ -1,19 +1,10 @@
 @amq-interconnect-1
 Feature: Configuration tests
 
-	#Scenario: Check that Router ID can be updated
-	# When container is started with env
-	# | variable     | value     |
-	# | ROUTER_MODE  | interior  |
-	#Then container log should contain Router started in Interior mode
-	
-  Scenario: Check that Router ID can be updated
+  Scenario: Check that Router config can be updated
     When container is started with env
        | variable     | value     |
        | ROUTER_ID    | Router.B  |
+       | ROUTER_MODE  | interior  |
     Then container log should contain Container Name: Router.B
-
-    # Scenario: Check that ports are available
-    #When container is ready
-    #Then check that port 56720 is open
-    #Then check that port 8080  is open
+    And container log should contain Router started in Interior mode
