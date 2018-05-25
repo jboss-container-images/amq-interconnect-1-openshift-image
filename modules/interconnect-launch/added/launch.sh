2a0796eeaf319170dda9a6fe3aaf50a950c8eff8
Gordon Sim <gsim@redhat.com>
2018-05-25 21:44:07 +0100

    refine the configure&launch scripts to provide a flexible interface to the image

--- modules/interconnect-launch/added/launch.sh
+++ modules/interconnect-launch/added/launch.sh
@@ -3,6 +3,10 @@
 
 CONFIG_FILE=${AMQ_HOME}/etc/qdrouterd.conf
 
-${AMQ_HOME}/bin/configure_interconnect.sh ${AMQ_HOME}
+${AMQ_HOME}/bin/configure_interconnect.sh ${AMQ_HOME} $CONFIG_FILE
 
-exec qdrouterd -c $CONFIG_FILE
+if [ -f $CONFIG_FILE ]; then
+    ARGS="-c $CONFIG_FILE"
+fi
+
+exec qdrouterd $ARGS
